151b0d56f7fae46b113e21452107d2541f966686
Gaikwad Rohan <rrgaikwad8005@gmail.com>
2026-07-20 15:30:07 +0530

    fix: set base path and router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { AppShell } from "./components/Layout/AppShell";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/resumebuild-ts/">
       <AppShell>
         <Routes>
           <Route path="/" element={<Dashboard />} />
